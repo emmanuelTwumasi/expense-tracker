5bd0a3867c47d62a670ff72cbc25223c6aefa876
emmanuelTwumasi <protwumasi@gmail.com>
2022-08-21 15:24:46 +0000

    move routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddExpenseComponent } from './component/add-expense/add-expense.component';
+import { ListExpenseComponent } from './component/list-expense/list-expense.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'expenses',component:ListExpenseComponent},
+  {path:'addexpense', component: AddExpenseComponent},
+  {path:'editexpense/:id', component:AddExpenseComponent},
+  {path:'',  redirectTo:  '/expenses' ,    pathMatch:  'full'}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
